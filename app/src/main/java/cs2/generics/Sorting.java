c01d7df70f2fd4c0683f9d3fc84abc2ae366c568
Matt Hibbs <mhibbs@trinity.edu>
2023-11-02 12:42:56 -0500

    Sorting methods

--- app/src/main/java/cs2/generics/Sorting.java
+++ app/src/main/java/cs2/generics/Sorting.java
@@ -14,6 +14,29 @@ public class Sorting {
     }
   }
 
+  public static void selectionSort(int[] a) {
+    for(int j=0; j<a.length; j++) {
+      int minIdx = j;
+      for(int i=j+1; i<a.length; i++) {
+        if(a[i] < a[minIdx]) { minIdx = i; }
+      }
+      //Swap smallest to the front
+      int tmp = a[j];
+      a[j] = a[minIdx];
+      a[minIdx] = tmp;
+    }
+  }
+  public static void insertionSort(int[] a) {
+    for(int j=1; j<a.length; j++) {
+      int tmp = a[j];
+      int i = j-1;
+      while(i >= 0 && a[i] > tmp) {
+        a[i+1] = a[i];
+        i--;
+      }
+      a[i+1] = tmp;
+    }
+  }
   public static void main(String[] args) {
     int[] x = new int[10];
     for(int i=0; i<x.length; i++) {
@@ -21,7 +44,9 @@ public class Sorting {
     }
     for(int a : x) { System.out.print(a + ","); }
     System.out.println();
-    bubbleSort(x);
+    insertionSort(x);
+    //selectionSort(x);
+    //bubbleSort(x);
     for(int a : x) { System.out.print(a + ","); }
     System.out.println();
 
